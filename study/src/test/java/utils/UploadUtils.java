9fecb27c5bf8feee5630278e6a763e262bf9fa1c
zhanhong1997 <https://github.com/zhanhong1997>
2022-08-04 22:39:00 +0800

    Add UploadUtils

--- study/src/test/java/utils/UploadUtils.java
+++ study/src/test/java/utils/UploadUtils.java
@@ -1,13 +1,18 @@
 package utils;
 
 
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
+import com.sun.xml.internal.ws.resources.SenderMessages;
 
 public class UploadUtils {
     public void uploadFile(String filePath){
-        WinDef.HWND hwnd = showWindow("&Open");
-
+        WinDef.HWND hwnd = showWindow("打开(&O)");
+        inputFilePath(hwnd,filePath);
+        clickButton(hwnd,"打开");
     }
 
     public WinDef.HWND showWindow(String windowTitle){
@@ -21,9 +26,18 @@ public class UploadUtils {
         return hwnd;
     }
 
+    public void inputFilePath( WinDef.HWND hwnd, String filePath){
+        WinDef.HWND comboBoxEx32 = User32.INSTANCE.FindWindowEx(hwnd,null,"ComboBoxEx32",null);
+        WinDef.HWND comboBox32 = User32.INSTANCE.FindWindowEx(comboBoxEx32,null,"ComboBox32",null);
+        WinDef.HWND edit = User32.INSTANCE.FindWindowEx(comboBox32,null,"Edit",null);
+        Pointer p = new Memory((long) Native.WCHAR_SIZE *(filePath.length() + 1));
+        p.setWideString(0,filePath);
+        User32.INSTANCE.SendMessage(edit,0x0c,new WinDef.WPARAM(0),new WinDef.LPARAM(Pointer.nativeValue(p)));
+    }
 
-
-    public void inputFilePath(String filePath, WinDef.HWND hwmd){
-        User32.INSTANCE.FindWindowEx(hwmd,null,"ComboBoxEx32",null);
+    public void clickButton(WinDef.HWND hwnd, String button){
+        WinDef.HWND open = User32.INSTANCE.FindWindowEx(hwnd,null,button,null);
+        User32.INSTANCE.SendMessage(open,0x201,null,null);
+        User32.INSTANCE.SendMessage(open,0x202,null,null);
     }
 }
